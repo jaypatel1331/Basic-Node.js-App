92257a90e5075193170bbaaccc8cd31dae648ebe
jaypatel1331 <82784261+jaypatel1331@users.noreply.github.com>
2021-11-04 10:48:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
-/*********************************************************************************
-* WEB322 – Assignment 1
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-* 
-* Name: Jay AShishbhai Patel
-* Student ID: 154925192
-* Date: 08 September 2021
-*
-* Online (Heroku) URL: https://guarded-shelf-01128.herokuapp.com/
-*
-********************************************************************************/ 
-
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
